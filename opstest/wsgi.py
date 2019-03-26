a7986bef6c419be4fdaacd359901c445fd664655
nalanxiao <nalanxiao@qq.com>
2019-03-26 14:23:51 +0800

    1

---
+++ opstest/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for opstest project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opstest.settings")
+
+application = get_wsgi_application()
