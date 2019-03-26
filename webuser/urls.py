fd8d599f81baddde57c35b51b99428a2cb9e31d9
nalanxiao <nalanxiao@qq.com>
2019-03-26 16:26:23 +0800

    update webuser

--- webuser/urls.py
+++ webuser/urls.py
@@ -14,6 +14,25 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Return a user instance.
+
+    list:
+        Return all users, ordered by most recently joined.
+
+    create:
+        Create a new user.
+
+    delete:
+        Remove an existing user.
+
+    partial_update:
+        Update one or more fields on an existing user.
+
+    update:
+        Update a user.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
